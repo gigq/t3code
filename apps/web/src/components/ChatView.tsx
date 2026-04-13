JohnZolton/t3code-orchestrator

Add OpenCode composer adapter support
- preserve OpenCode variant and agent selections
- hide unsupported interaction mode controls
- add provider snapshot and UI coverage

apps/web/src/components/ChatView.tsx
@@ -169,6 +169,7 @@ import { ComposerPendingApprovalPanel } from "./chat/ComposerPendingApprovalPane
 import { ComposerPendingUserInputPanel } from "./chat/ComposerPendingUserInputPanel";
 import { ComposerPlanFollowUpBanner } from "./chat/ComposerPlanFollowUpBanner";
 import {
+  getComposerProviderControls,
   getComposerProviderState,
   renderProviderTraitsMenuContent,
   renderProviderTraitsPicker,
@@ -1023,6 +1024,10 @@ export default function ChatView({ threadId }: ChatViewProps) {
         modelOptions: composerModelOptions,
       }),
     [composerModelOptions, prompt, selectedModel, selectedProvider, selectedProviderModels],
+  );
+  const composerProviderControls = useMemo(
+    () => getComposerProviderControls(selectedProvider),
+    [selectedProvider],
   );
   const selectedPromptEffort = composerProviderState.promptEffort;
   const selectedModelOptionsForDispatch = composerProviderState.modelOptionsForDispatch;
@@ -4260,6 +4265,9 @@ export default function ChatView({ threadId }: ChatViewProps) {
                             interactionMode={interactionMode}
                             planSidebarOpen={planSidebarOpen}
                             runtimeMode={runtimeMode}
+                            showInteractionModeToggle={
+                              composerProviderControls.showInteractionModeToggle
+                            }
                             traitsMenuContent={providerTraitsMenuContent}
                             onToggleInteractionMode={toggleInteractionMode}
                             onTogglePlanSidebar={togglePlanSidebar}
@@ -4282,28 +4290,32 @@ export default function ChatView({ threadId }: ChatViewProps) {
                               className="mx-0.5 hidden h-4 sm:block"
                             />
 
-                            <Button
-                              variant="ghost"
-                              className="shrink-0 whitespace-nowrap px-2 text-muted-foreground/70 hover:text-foreground/80 sm:px-3"
-                              size="sm"
-                              type="button"
-                              onClick={toggleInteractionMode}
-                              title={
-                                interactionMode === "plan"
-                                  ? "Plan mode — click to return to normal build mode"
-                                  : "Default mode — click to enter plan mode"
-                              }
-                            >
-                              <BotIcon />
-                              <span className="sr-only sm:not-sr-only">
-                                {interactionMode === "plan" ? "Plan" : "Build"}
-                              </span>
-                            </Button>
+                            {composerProviderControls.showInteractionModeToggle ? (
+                              <>
+                                <Button
+                                  variant="ghost"
+                                  className="shrink-0 whitespace-nowrap px-2 text-muted-foreground/70 hover:text-foreground/80 sm:px-3"
+                                  size="sm"
+                                  type="button"
+                                  onClick={toggleInteractionMode}
+                                  title={
+                                    interactionMode === "plan"
+                                      ? "Plan mode — click to return to normal build mode"
+                                      : "Default mode — click to enter plan mode"
+                                  }
+                                >
+                                  <BotIcon />
+                                  <span className="sr-only sm:not-sr-only">
+                                    {interactionMode === "plan" ? "Plan" : "Build"}
+                                  </span>
+                                </Button>
 
-                            <Separator
-                              orientation="vertical"
-                              className="mx-0.5 hidden h-4 sm:block"
-                            />
+                                <Separator
+                                  orientation="vertical"
+                                  className="mx-0.5 hidden h-4 sm:block"
+                                />
+                              </>
+                            ) : null}
 
                             <Button
                               variant="ghost"

apps/web/src/components/chat/CompactComposerControlsMenu.browser.tsx
@@ -117,6 +117,7 @@ async function mountMenu(props?: { modelSelection?: ModelSelection; prompt?: str
       interactionMode="default"
       planSidebarOpen={false}
       runtimeMode="approval-required"
+      showInteractionModeToggle
       traitsMenuContent={
         <TraitsMenuContent
           provider={provider}
@@ -257,5 +258,38 @@ describe("CompactComposerControlsMenu", () => {
         'Your prompt contains "ultrathink" in the text. Remove it to change effort.',
       );
     });
+  });
+
+  it("can hide the interaction mode section", async () => {
+    const host = document.createElement("div");
+    document.body.append(host);
+    const screen = await render(
+      <CompactComposerControlsMenu
+        activePlan={false}
+        interactionMode="default"
+        planSidebarOpen={false}
+        runtimeMode="approval-required"
+        showInteractionModeToggle={false}
+        onToggleInteractionMode={vi.fn()}
+        onTogglePlanSidebar={vi.fn()}
+        onToggleRuntimeMode={vi.fn()}
+      />,
+      { container: host },
+    );
+
+    await page.getByLabelText("More composer controls").click();
+
+    await vi.waitFor(() => {
+      const text = document.body.textContent ?? "";
+      expect(text).not.toContain("Mode");
+      expect(text).not.toContain("Chat");
+      expect(text).not.toContain("Plan");
+      expect(text).toContain("Access");
+      expect(text).toContain("Supervised");
+      expect(text).toContain("Full access");
+    });
+
+    await screen.unmount();
+    host.remove();
   });
 });

gigq/t3code

apps/web/src/components/ChatView.tsx
@@ -180,6 +180,7 @@ import { ComposerPendingApprovalPanel } from "./chat/ComposerPendingApprovalPane
 import { ComposerPendingUserInputPanel } from "./chat/ComposerPendingUserInputPanel";
 import { ComposerPlanFollowUpBanner } from "./chat/ComposerPlanFollowUpBanner";
 import {
+  getComposerProviderControls,
   getComposerProviderState,
   renderProviderTraitsMenuContent,
   renderProviderTraitsPicker,
@@ -1147,6 +1148,10 @@ export default function ChatView({ threadId }: ChatViewProps) {
         modelOptions: composerModelOptions,
       }),
     [composerModelOptions, prompt, selectedModel, selectedProvider, selectedProviderModels],
+  );
+  const composerProviderControls = useMemo(
+    () => getComposerProviderControls(selectedProvider),
+    [selectedProvider],
   );
   const selectedPromptEffort = composerProviderState.promptEffort;
   const selectedModelOptionsForDispatch = composerProviderState.modelOptionsForDispatch;
@@ -4566,6 +4571,9 @@ export default function ChatView({ threadId }: ChatViewProps) {
                             autoDeferUntil={autoDeferUntil}
                             planSidebarOpen={planSidebarOpen}
                             runtimeMode={runtimeMode}
+                            showInteractionModeToggle={
+                              composerProviderControls.showInteractionModeToggle
+                            }
                             traitsMenuContent={providerTraitsMenuContent}
                             onSelectInteractionMode={handleInteractionModeChange}
                             {...(isServerThread
@@ -4594,29 +4602,33 @@ export default function ChatView({ threadId }: ChatViewProps) {
                               className="mx-0.5 hidden h-4 sm:block"
                             />
 
-                            <Button
-                              variant="ghost"
-                              className={cn(
-                                "shrink-0 whitespace-nowrap px-2 sm:px-3",
-                                autoModeDeferred
-                                  ? "bg-amber-100/70 text-amber-900 hover:bg-amber-200/80 hover:text-amber-950"
-                                  : "text-muted-foreground/70 hover:text-foreground/80",
-                              )}
-                              size="sm"
-                              type="button"
-                              onClick={cycleInteractionMode}
-                              title={interactionModeTitle(interactionMode, autoDeferUntil)}
-                            >
-                              <BotIcon />
-                              <span className="sr-only sm:not-sr-only">
-                                {interactionModeLabel(interactionMode, autoDeferUntil)}
-                              </span>
-                            </Button>
+                            {composerProviderControls.showInteractionModeToggle ? (
+                              <>
+                                <Button
+                                  variant="ghost"
+                                  className={cn(
+                                    "shrink-0 whitespace-nowrap px-2 sm:px-3",
+                                    autoModeDeferred
+                                      ? "bg-amber-100/70 text-amber-900 hover:bg-amber-200/80 hover:text-amber-950"
+                                      : "text-muted-foreground/70 hover:text-foreground/80",
+                                  )}
+                                  size="sm"
+                                  type="button"
+                                  onClick={cycleInteractionMode}
+                                  title={interactionModeTitle(interactionMode, autoDeferUntil)}
+                                >
+                                  <BotIcon />
+                                  <span className="sr-only sm:not-sr-only">
+                                    {interactionModeLabel(interactionMode, autoDeferUntil)}
+                                  </span>
+                                </Button>
 
-                            <Separator
-                              orientation="vertical"
-                              className="mx-0.5 hidden h-4 sm:block"
-                            />
+                                <Separator
+                                  orientation="vertical"
+                                  className="mx-0.5 hidden h-4 sm:block"
+                                />
+                              </>
+                            ) : null}
 
                             <Button
                               variant="ghost"

apps/web/src/components/chat/CompactComposerControlsMenu.browser.tsx
@@ -118,6 +118,7 @@ async function mountMenu(props?: { modelSelection?: ModelSelection; prompt?: str
       autoDeferUntil={null}
       planSidebarOpen={false}
       runtimeMode="approval-required"
+      showInteractionModeToggle
       traitsMenuContent={
         <TraitsMenuContent
           provider={provider}
@@ -272,5 +273,40 @@ describe("CompactComposerControlsMenu", () => {
         'Your prompt contains "ultrathink" in the text. Remove it to change effort.',
       );
     });
+  });
+
+  it("can hide the interaction mode section", async () => {
+    const host = document.createElement("div");
+    document.body.append(host);
+    const screen = await render(
+      <CompactComposerControlsMenu
+        activePlan={false}
+        interactionMode="default"
+        autoDeferUntil={null}
+        planSidebarOpen={false}
+        runtimeMode="approval-required"
+        showInteractionModeToggle={false}
+        onSelectInteractionMode={vi.fn()}
+        onSetAutoDeferUntil={vi.fn()}
+        onTogglePlanSidebar={vi.fn()}
+        onToggleRuntimeMode={vi.fn()}
+      />,
+      { container: host },
+    );
+
+    await page.getByLabelText("More composer controls").click();
+
+    await vi.waitFor(() => {
+      const text = document.body.textContent ?? "";
+      expect(text).not.toContain("Mode");
+      expect(text).not.toContain("Chat");
+      expect(text).not.toContain("Plan");
+      expect(text).toContain("Access");
+      expect(text).toContain("Supervised");
+      expect(text).toContain("Full access");
+    });
+
+    await screen.unmount();
+    host.remove();
   });
 });